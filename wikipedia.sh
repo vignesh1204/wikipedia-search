ab036a19d09594e094c9ac990f61da59078bab6d
vignesh1204 <vignesh1999@gmail.com>
2021-03-04 20:27:57 +0530

    created wikipedia.sh

---
+++ wikipedia.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    read -p 'Enter search term: ' keyword
+    read -p 'Enter name of log file (default - logfile.txt): ' log
+fi
+
+while getopts ":s:l:" options; do
+    case "${options}" in
+        s)
+            keyword="$OPTARG"
+            ;;
+        l)
+            log="$OPTARG"
+            ;;
+        :)
+            echo "Error: -${OPTARG} requires an argument."
+            exit 1
+            ;;
+        ?)
+            echo "Script usage: $(basename $0) [-s <search term>] [-l <log file name>]" 1>&2
+            exit 1
+            ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+if [ -z "$log" ]
+then
+    log=logfile.txt
+fi
+
+if [ -z "$keyword" ]
+then
+    echo "Keyword cannot be empty"
+fi
+
+keyword2=${keyword// /%20}
+
+url=https://en.wikipedia.org/wiki/$keyword2
+
+echo -ne '#######                        (25%)\r'
+sleep 1
+echo -ne '##############                 (50%)\r'
+sleep 1
+echo -ne '#####################          (75%)\r'
+sleep 1
+echo -ne '############################   (100%)\r'
+echo -ne '\n'
+
+echo "$(date) - $keyword - $url" >> $log
+
+echo "done"
